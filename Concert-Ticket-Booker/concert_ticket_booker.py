26ea3e8c1bad0d9d01b723c8f592104aa6a38d5f
Sinethemba Madondo <sne01madondo@gmail.com>
2026-07-20 17:09:16 +0200

    Add files via upload

---
+++ Concert-Ticket-Booker/concert_ticket_booker.py
@@ -0,0 +1,8 @@
+#Digital Ticket Counter
+print("Welcome to Ticket Nova, where tickets are one click away!")
+print("To book tickets please follow the following prompts.")
+full_name = input("Enter your full name: ")
+band_or_artist = input("Enter your desired band or artist: ")
+date_of_performance = input("Enter the date of the performance: ")
+print(f"Congratulations, {full_name} . You have successfully booked tickets to see {band_or_artist} on the {date_of_performance}")
+print("Please tap the download button to save the ticket on your device.")
